ae824ae9c2b881de2a980faaf72133a7494dc02d
wlove <lalakasuper2@gmail.com>
2024-07-30 21:17:31 +0300

    editet nginx config

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -15,3 +15,5 @@ rm -rf /etc/nginx/key.pem
 ## копируем сертификаты из образа certbot в папку Nginx
 #cp /etc/letsencrypt/live/certfolder*/fullchain.pem /etc/nginx/cert.pem
 #cp /etc/letsencrypt/live/certfolder*/privkey.pem /etc/nginx/key.pem
+
+ls
