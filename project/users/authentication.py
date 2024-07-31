0c7cfe58ac1d30ce4f52a62d51f492480b3e6ea9
wlovem <lalakasuper2@gmail.com>
2024-07-31 12:14:34 +0700

    added username translit

--- project/users/authentication.py
+++ project/users/authentication.py
@@ -1,10 +1,10 @@
 import requests
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
-import jwt
 from users.models import User
 from django.db.models import Q
 from projects.models import Project
+from transliterate import translit
 
 
 class CustomTokenAuthentication(BaseAuthentication):
@@ -27,15 +27,16 @@ class CustomTokenAuthentication(BaseAuthentication):
             user = User.objects.filter(filter).first()
             if user:
                 return (user, uuid)
-        except :
+        except:
             pass
-        headers = {}
-        headers["Authorization"] = f"Bearer {project.amo_token}"
+        headers = {
+            "Authorization": f"Bearer {project.amo_token}"
+        }
         params = {
             'with': "uuid"
         }
         response = requests.get(f"https://{project.suburl}.amocrm.ru/api/v4/users",
-                         headers=headers, params=params)
+                                headers=headers, params=params)
         if response.status_code != 200:
             raise AuthenticationFailed('Cant connect to project')
         try:
@@ -48,11 +49,11 @@ class CustomTokenAuthentication(BaseAuthentication):
         except:
             raise AuthenticationFailed('User not found')
 
-
     def get_user(self, user_id, user_uuid, username, project):
         try:
             user = User.objects.get(amo_id=user_id)
         except User.DoesNotExist:
+            username = translit(username.replace(' ', '_'), language_code='ru', reversed=True)
             user = User.objects.create_user(amo_id=user_id,
                                             username=username,
                                             amo_uuid=user_uuid,
