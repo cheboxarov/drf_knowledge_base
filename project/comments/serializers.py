af9581b98e446d567ec10fdb5e4dc1a7f54105dd
wlovem <lalakasuper2@gmail.com>
2024-08-15 14:29:14 +0700

    добавил приложение для комментов

--- project/comments/serializers.py
+++ project/comments/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 from .models import Comment
 
-
 class CommentSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Comment
-        fields = ("id", "author_id", "date_created", "text", "article")
+        fields = ['id', 'author_id', 'date_created', 'text', 'article']
+        read_only_fields = ['author_id', 'article']
