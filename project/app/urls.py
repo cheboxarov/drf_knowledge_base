d9c2fd3bf36f7b6a2cad45564408f46c04ff03a1
wlove <lalakasuper2@gmail.com>
2024-10-02 15:15:42 +0700

    Обернул все урлсы в knowledge

--- project/app/urls.py
+++ project/app/urls.py
@@ -33,8 +33,10 @@ articles_router = NestedSimpleRouter(router, r"articles", lookup="article")
 articles_router.register(r"comments", CommentsViewSet, basename="article-comments")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("api/v1/", include(router.urls)),
-    path("api/v1/", include(articles_router.urls)),
+    path('knowledge', include([
+        path("admin/", admin.site.urls),
+        path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path("api/v1/", include(router.urls)),
+        path("api/v1/", include(articles_router.urls)),
+    ]))
 ]
